82003a23e0b96423b44925a2cb74183babe626fe
kx1nQyy <loginovt13@gmail.com>
2024-10-20 13:09:23 +0300

    clear

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import {generateAds} from "./data.js";
 import {POINTS} from "./const.js";
 import {mapInit} from "./map.js";
 
-const ADVERTISMENT_COUNT = 2;
-
 const ads = generateAds(POINTS.khv);
 const map = mapInit();
 addMarkers(ads, map);
