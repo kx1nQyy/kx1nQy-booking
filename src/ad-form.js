596c78a33c7e1cf71267bef257c88e235f9ecc6a
kx1nQyy <loginovt13@gmail.com>
2024-11-03 01:02:45 +0300

    Changelog

--- src/ad-form.js
+++ src/ad-form.js
@@ -1,1 +1,60 @@
-const formElement = document.querySelector('.ad-form');
+const adFormElement = document.querySelector('.ad-form');
+const titleInputELment = adFormElement.querySelector('#title');
+const priceInputELment = adFormElement.querySelector('#price');
+const descriptionInputELment = adFormElement.querySelector('#description');
+
+const MAX_PRICE_VALUE = 10000;
+const MIN_PRICE_VALUE = 1;
+const MAX_TITLE_LENGTH = 15;
+const MAX_DESCRIPTION_LENGTH = 100;
+const MIN_DESCRIPTION_LENGTH = 10;
+
+if (priceInputELment) {
+    priceInputELment.addEventListener('input', () => {
+        const valuePrice = priceInputELment.value;
+
+        let error = '';
+        if (valuePrice > MAX_PRICE_VALUE) {
+            error = `${MAX_PRICE_VALUE}р. максимаьная стоимость.`;
+        }
+        else if (valuePrice < MIN_PRICE_VALUE) {
+            error = `${MIN_PRICE_VALUE}р. минимальная стоимость.`;
+        }
+
+        priceInputELment.setCustomValidity(error);
+        priceInputELment.reportValidity();
+    });
+}
+
+if (titleInputELment) {
+    titleInputELment.addEventListener('input', () => {
+        const valueLength = titleInputELment.value.length;
+
+        let error = '';
+        if (valueLength > MAX_TITLE_LENGTH) {
+            error = `Удалите лишние ${valueLength - MAX_TITLE_LENGTH} симв.`;
+        }
+
+        titleInputELment.setCustomValidity(error);
+        titleInputELment.reportValidity();
+    }); 
+}
+
+if (descriptionInputELment) {
+    descriptionInputELment.addEventListener('input', () => {
+        const valueLength = descriptionInputELment.value.length;
+
+        let error = '';
+        if (valueLength > MAX_DESCRIPTION_LENGTH) {
+            error = `Удалите лишние ${valueLength - MAX_DESCRIPTION_LENGTH} симв.`;
+        }
+        else if (valueLength < MIN_DESCRIPTION_LENGTH) {
+            error = `${MIN_DESCRIPTION_LENGTH} минимальное число символов для описания.`
+        }
+
+
+        console.log(descriptionInputELment.setCustomValidity(error))
+        descriptionInputELment.setCustomValidity(error);
+        descriptionInputELment.reportValidity();
+    });
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import {addMarkers} from "./marker";
-import {generateAds} from "./data.js";
 import {POINTS} from "./const.js";
+import {addMarkers} from "./marker.js";
+import {generateAds} from "./data.js";
 import {mapInit} from "./map.js";
+import './ad-form.js';
 
 const ads = generateAds(POINTS.khv);
 const map = mapInit();
